ed4a4eeaa95b9d82ed06eb29a4986a09f0050e74
bkis <boerge.kiss@gmail.com>
2014-10-29 19:15:37 +0100

    added demo case for matrix search

--- IR-Shakespeare/src/bk/ir/Main.java
+++ IR-Shakespeare/src/bk/ir/Main.java
@@ -1,10 +1,19 @@
 package bk.ir;
 
+import bk.ir.search.matrix.MatrixSearch;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		MatrixSearch ms = new MatrixSearch(new Corpus("shakespeare.txt", "1[56][0-9]{2}\n"));
+		ms.search("Brutus Caesar");
+		ms.search("house");
+		ms.search("hand");
+		ms.search("dagger");
+		ms.search("murp");
+		ms.search("fatal");
+		ms.search("fetus");
+		ms.search("game");
 	}
 
 }
