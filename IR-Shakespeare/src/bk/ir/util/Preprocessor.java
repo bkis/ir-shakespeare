245f85edc19919e454ec0a9f865804f8d4ce0873
bkis <boerge.kiss@gmail.com>
2014-11-05 13:18:16 +0100

    preprocessor

--- IR-Shakespeare/src/bk/ir/util/Preprocessor.java
+++ IR-Shakespeare/src/bk/ir/util/Preprocessor.java
@@ -3,13 +3,13 @@ package bk.ir.util;
 import java.util.Arrays;
 import java.util.List;
 
-public class Tokenizer {
+public class Preprocessor {
 	
 	public static final String SPLIT_NON_WORD = "\\P{L}+";
 
 	
 	public static List<String> tokenize(String input, String pattern){
-		return Arrays.asList(input.split(pattern));
+		return Arrays.asList(input.toLowerCase().split(pattern.toLowerCase()));
 	}
 
 }
